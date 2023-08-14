b520a4a93eb3ba0431b9cddd6445c06d86a83594
timsonau <117775073+timsonau@users.noreply.github.com>
2023-08-13 20:55:22 -0500

    Refactoring as well as code cleanup

--- pages/components/CubeApp.tsx
+++ pages/components/CubeApp.tsx
@@ -1,18 +1,26 @@
 import CubeMap from "./CubeMap";
 import CubeForm from "./CubeForm";
 import CubeSolution from "./CubeSolution";
-import React, {useState} from "react";
+import React, { useState } from "react";
+
+import { DEFAULT_CUBE } from "../util/constants";
 
 function CubeApp() {
-    const [cubeString, setCubeString] = useState('bbbbbbbbbrrrrrrrrrgggggggggoooooooooyyyyyyyyywwwwwwwww')
-    const [solution, setSolution] = useState('')
-    console.log("Cube App Rendered")
-    return (
-        <div className="cube-app">
-            <CubeForm setCubeString={setCubeString} cubeString={cubeString} setSolution={setSolution}/>
-            <CubeMap cubeString={cubeString}/>
-            <CubeSolution solution={solution}/>
-        </div>
-    )
+  /* State Variables */
+  const [cubeString, setCubeString] = useState(DEFAULT_CUBE);
+  const [solution, setSolution] = useState("");
+
+  console.log("Cube App Rendered");
+  return (
+    <div className="cube-app">
+      <CubeForm
+        setCubeString={setCubeString}
+        cubeString={cubeString}
+        setSolution={setSolution}
+      />
+      <CubeMap cubeString={cubeString} />
+      <CubeSolution solution={solution} />
+    </div>
+  );
 }
-export default CubeApp
+export default CubeApp;
