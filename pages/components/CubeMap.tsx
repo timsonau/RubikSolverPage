2fffcb0aae8e0605d627e2437f68350f0a6f97ce
timsonau <117775073+timsonau@users.noreply.github.com>
2023-08-14 21:03:59 -0500

    Responsive design

--- pages/components/CubeMap.tsx
+++ pages/components/CubeMap.tsx
@@ -14,15 +14,17 @@ function CubeMap(props: CubeMapProps) {
   if (!props.cubeString) props.cubeString = DEFAULT_CUBE;
 
   return (
-    <div className="cube-mapping">
-      <CubeFace side="up" colors={props.cubeString.substring(36, 45)} />
-      <div className="side-faces flex">
-        <CubeFace side="left" colors={props.cubeString.substring(27, 36)} />
-        <CubeFace side="front" colors={props.cubeString.substring(0, 9)} />
-        <CubeFace side="right" colors={props.cubeString.substring(9, 18)} />
-        <CubeFace side="back" colors={props.cubeString.substring(18, 27)} />
+    <div className="cube-map-wrapper">
+      <div className="cube-map">
+        <CubeFace side="up" colors={props.cubeString.substring(36, 45)} />
+        <div className="side-faces flex">
+          <CubeFace side="left" colors={props.cubeString.substring(27, 36)} />
+          <CubeFace side="front" colors={props.cubeString.substring(0, 9)} />
+          <CubeFace side="right" colors={props.cubeString.substring(9, 18)} />
+          <CubeFace side="back" colors={props.cubeString.substring(18, 27)} />
+        </div>
+        <CubeFace side="down" colors={props.cubeString.substring(45, 54)} />
       </div>
-      <CubeFace side="down" colors={props.cubeString.substring(45, 54)} />
     </div>
   );
 }
